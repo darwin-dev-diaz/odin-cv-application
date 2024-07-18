8ab646d81b6ba864be870c5c6d847b478a663807
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-07-18 11:15:39 -0400

    Progress accordian page component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import AccordianPage  from './components/AccordianPage'
+import "./styles/reset.css"
+import "./styles/main.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AccordianPage 
+      title="Personal Info"/>
   </React.StrictMode>,
 )
