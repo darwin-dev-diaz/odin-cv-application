c725fcb27f609cc39f1c070e82453fc32887a96a
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-07-28 12:15:25 -0400

    Add the ability to close an open accordion page

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -10,26 +10,35 @@ import "../styles/Accordion.css";
 export default function Accordion() {
   const [openID, setOpenID] = useState(0);
 
+  function handleAccordionOpen(id){
+    if(openID === id){
+      setOpenID(4)
+    } else {
+      setOpenID(id);
+    }
+
+  }
+
   return (
     <div className="accordion-container">
       <AccordionPage
         title="Personal Info"
         open={openID === 0}
-        onOpen={() => setOpenID(0)}
+        onOpen={()=>handleAccordionOpen(0)}
       >
         <PersonalInfoField />
       </AccordionPage>
       <AccordionPage
         title="Education"
         open={openID === 1}
-        onOpen={() => setOpenID(1)}
+        onOpen={()=>handleAccordionOpen(1)}
       >
         <EducationField />
       </AccordionPage>
       <AccordionPage
         title="Experience"
         open={openID === 2}
-        onOpen={() => setOpenID(2)}
+        onOpen={()=>handleAccordionOpen(2)}
       >
         <ExperiencePage />
       </AccordionPage>
